ebed1f50b889784dd5a85b8bc93dd30ff6b91ff0
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2024-12-13 21:38:02 +0600

    feat(TD-8): Set up controller and database

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem "sqlite3", ">= 2.1"
 gem "puma", ">= 5.0"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
-
+gem 'pg', '~> 1.4'
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,55 @@
-class ApplicationController < ActionController::API
-end
+module Api
+    module V1
+      class TasksController < ApplicationController
+        before_action :set_task, only: [:show, :update, :destroy]
+  
+        # GET /api/v1/tasks
+        def index
+          tasks = Task.all
+          render json: tasks
+        end
+  
+        # GET /api/v1/tasks/:id
+        def show
+          render json: @task
+        end
+  
+        # POST /api/v1/tasks
+        def create
+          task = Task.new(task_params)
+          if task.save
+            render json: task, status: :created
+          else
+            render json: { error: task.errors.full_messages }, status: :unprocessable_entity
+          end
+        end
+  
+        # PUT /api/v1/tasks/:id
+        def update
+          if @task.update(task_params)
+            render json: @task
+          else
+            render json: { error: @task.errors.full_messages }, status: :unprocessable_entity
+          end
+        end
+  
+        # DELETE /api/v1/tasks/:id
+        def destroy
+          @task.destroy
+          head :no_content
+        end
+  
+        private
+  
+        def set_task
+          @task = Task.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          render json: { error: "Task not found" }, status: :not_found
+        end
+  
+        def task_params
+          params.require(:task).permit(:title, :completed)
+        end
+      end
+    end
+  end  
