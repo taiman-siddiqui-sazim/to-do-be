4605760e83065ae4d5bb7519e6c3ba9e3299cd50
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2024-12-19 02:21:59 +0700

    Add feature to delete tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :tasks, only: [:index, :show, :create, :update] do
-        member do
-          patch :update_completion
-        end
+      resources :tasks, only: [:index, :show, :create, :update, :destroy] do
+        patch :update_completion, on: :member
       end
     end
   end
