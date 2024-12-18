ff502fed8d45ffe7ae380f6bbb673e0579f34834
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2024-12-18 23:52:31 +0700

    feat(TD-15): Add valdation logic to task completion update

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -61,22 +61,32 @@ module Api
       end 
       
       def update_completion
-        if params[:task].key?(:completed)
-          @task.completed = params[:task][:completed]
-          if @task.save
-            render json: TaskSerializer.serialize(
-              @task,
-              metadata: metadata
-            ), status: :ok
-          else
-            render json: TaskSerializer.serialize_errors(
-              @task.errors.full_messages,
-              metadata: metadata
-            ), status: :unprocessable_entity
-          end
+        if params[:task].keys != ['completed']
+          render json: TaskSerializer.serialize_errors(
+            ['Only the completed field can be updated'],
+            metadata: metadata
+          ), status: :unprocessable_entity
+          return
+        end
+
+        completed_value = params[:task][:completed]
+        unless [true, false].include?(completed_value)
+          render json: TaskSerializer.serialize_errors(
+            ['Completed must be true or false'],
+            metadata: metadata
+          ), status: :unprocessable_entity
+          return
+        end
+
+        @task.completed = completed_value
+        if @task.save
+          render json: TaskSerializer.serialize(
+            @task,
+            metadata: metadata
+          ), status: :ok
         else
           render json: TaskSerializer.serialize_errors(
-            ['Completed status is required'],
+            @task.errors.full_messages,
             metadata: metadata
           ), status: :unprocessable_entity
         end
