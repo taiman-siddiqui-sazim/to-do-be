6dd761ae03826ccd73442e34199b92996eff347b
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2024-12-25 19:21:46 +0600

    feat(TD-21): Edit tasks in the backend (#6)
    * feat(TD-8): Set up controller and database
    
    * feat(TD-8): Add backend logic to create and get tasks
    
    * feat(TD-8): Handle task validation and output serialization
    
    * feat(TD-8): Update pagination logic and cross origin requests
    
    * fix(TD-8): Remove hardcode
    
    * feat(TD-21): Functionality to edit tasks
    
    * feat(TD-15): Add feature to update task complete status
    
    * feat(TD-15): Add valdation logic to task completion update
    
    * feat(TD-8): Set up controller and database
    
    * feat(TD-8): Add backend logic to create and get tasks
    
    * feat(TD-21): Functionality to edit tasks
    
    * feat(TD-15): Add strong params for update_completion method
    
    * Add feature to delete tasks (#8)

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class TasksController < ApplicationController
-      before_action :set_task, only: [:show]
+      before_action :set_task, only: [:show, :update, :update_completion, :destroy]
 
       def index
         result = PaginationService.paginate(Task, params)
@@ -35,6 +35,66 @@ module Api
         end
       end
 
+      def update
+        if params[:task].keys.any? { |key| !task_update_params.key?(key) }
+          render json: TaskSerializer.serialize_errors(
+            ['Only task title can be updated'],
+            metadata: metadata
+          ), status: :unprocessable_entity
+          return
+        end
+
+        @task.assign_attributes(task_update_params)
+        validator = TaskValidator.new(@task)
+
+        if validator.valid? && @task.save
+          render json: TaskSerializer.serialize(
+            @task,
+            metadata: metadata
+          ), status: :ok
+        else
+          render json: TaskSerializer.serialize_errors(
+            validator.errors.presence || @task.errors.full_messages,
+            metadata: metadata
+          ), status: :unprocessable_entity
+        end
+      end
+
+      def update_completion
+        if params[:task].keys.any? { |key| !completion_update_params.key?(key) }
+          render json: TaskSerializer.serialize_errors(
+            ['Only the completed field can be updated'],
+            metadata: metadata
+          ), status: :unprocessable_entity
+          return
+        end
+
+        @task.assign_attributes(completion_update_params)
+
+        if @task.save
+          render json: TaskSerializer.serialize(
+            @task,
+            metadata: metadata
+          ), status: :ok
+        else
+          render json: TaskSerializer.serialize_errors(
+            @task.errors.full_messages,
+            metadata: metadata
+          ), status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @task.destroy
+          render json: { message: "Task successfully deleted", metadata: metadata }, status: :ok
+        else
+          render json: TaskSerializer.serialize_errors(
+            @task.errors.full_messages,
+            metadata: metadata
+          ), status: :unprocessable_entity
+        end
+      end
+
       private
 
       def set_task
@@ -43,6 +103,14 @@ module Api
 
       def task_params
         params.require(:task).permit(:title, :completed)
+      end
+
+      def task_update_params
+        params.require(:task).permit(:title)
+      end
+
+      def completion_update_params
+        params.require(:task).permit(:completed)
       end
 
       def build_metadata(extra_metadata = {})
--- app/validators/task_validator.rb
+++ app/validators/task_validator.rb
@@ -15,7 +15,11 @@ class TaskValidator
   private
 
   def validate_title
+<<<<<<< HEAD
     if @task.title.blank? || !@task.title.is_a?(String)
+=======
+    if @task.title.blank? || !@task.title.is_a?(String) || @task.title.strip.empty?
+>>>>>>> TD-15-task-complete
       @errors << "Title must be a non-empty string"
     end
   end
